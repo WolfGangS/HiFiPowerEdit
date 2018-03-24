c27eef328a973d4ec02a3fdbd04dff3dc897d482
WolfGang <flamin2k8@gmail.com>
2018-03-24 00:55:41 +0000

    hifi fixes

--- html/js/powerEdit.js
+++ html/js/powerEdit.js
@@ -19,6 +19,7 @@ on  30/9/2017
 
 */
 (function() {
+    //window.setInterval(function(){console.log("boop");},500);
     var options = {
         mode: "tree",
         schema: EntitySchema,
@@ -29,25 +30,26 @@ on  30/9/2017
         sortObjectKeys: true,
         onError: onError
     };
+    var webLink = null;
     var container = document.getElementById("jsoneditor");
     var editor = new JSONEditor(container, options);
     var jsonData = null;
     var isSafe = false;
 
 
-    function addButtons(){
+    function addButtons() {
         $(".jsoneditor-menu").prepend(
             $("<button>").on("click", function() {
                 webLink.send("picker", {});
-            }).attr("title","Select Entity")
+            }).attr("title", "Select Entity")
         );
-        $("<button>").on("click",function(){
+        $("<button>").on("click", function() {
             safeSendDiff();
-        }).text("Apply").attr("style","width:auto;background-position:-96px -120px;padding:0px 5px 0px 5px;").insertBefore("table.jsoneditor-search");
+        }).text("Apply").attr("style", "width:auto;background-position:-96px -120px;padding:0px 5px 0px 5px;").insertBefore("table.jsoneditor-search");
 
-        $("<button>").on("click",function(){
+        $("<button>").on("click", function() {
             webLink.send("jsonDataRequest", jsonData.id);
-        }).text("Refresh").attr("style","width:auto;background-position:-96px -120px;padding:0px 5px 0px 5px;").insertBefore("table.jsoneditor-search");
+        }).text("Refresh").attr("style", "width:auto;background-position:-96px -120px;padding:0px 5px 0px 5px;").insertBefore("table.jsoneditor-search");
     }
 
     addButtons();
@@ -70,11 +72,11 @@ on  30/9/2017
         safeSendDiff(false);
     }
 
-    function safeSendDiff(force){
+    function safeSendDiff(force) {
         editor.validate();
         if (editor.errorNodes.length < 1) {
             var nd = editor.get();
-            if(force){
+            if (force) {
                 webLink.send("jsonData", { id: jsonData.id, props: nd });
                 jsonData = nd;
             } else {
@@ -82,7 +84,7 @@ on  30/9/2017
                 if (Object.keys(dif).length > 0) {
                     webLink.send("jsonData", { id: jsonData.id, props: dif });
                     jsonData = nd;
-                    if(dif.hasOwnProperty("locked")){
+                    if (dif.hasOwnProperty("locked")) {
                         webLink.send("jsonDataRequest", jsonData.id);
                     }
                 }
@@ -91,21 +93,53 @@ on  30/9/2017
     }
 
     // get json
-    //var json = editor.get();
+    //var json = editor.get();EventBridge.scriptEventReceived
+    document.addEventListener('DOMContentLoaded', function() {
+        EventBridge.scriptEventReceived.connect(scriptIn);
+    });
 
-    var webLink = new QTWebLink();
+    function scriptIn(data) {
+        data = JSON.parse(data);
+        if (!(data instanceof Array)) data = [data];
+        console.log(data);
+        var e;
+        for (var i in data) {
+            e = data[i];
+            if (e.hasOwnProperty("type") && e.hasOwnProperty("data")) {
+                e.type = e.type.toLowerCase();
+                switch (e.type) {
+                    case "jsondata":
+                        isSafe = false;
+                        jsonData = e.data;
+                        console.log(e.data);
+                        editor.set(jsonData);
+                        editor.expandAll();
+                        $(".jsoneditor-contextmenu").remove();
+                        //lockID();
+                        window.setTimeout(function() {
+                            isSafe = true;
+                        }, 1000);
+                        break;
+                }
+            }
+        }
+    }
+    /*
+    webLink = new QTWebLink();
     webLink.start(function(e) {
         webLink.regCallback("jsonData", function(data) {
+            console.log(data);
             isSafe = false;
             jsonData = data;
             editor.set(jsonData);
+            editor.expandAll();
             $(".jsoneditor-contextmenu").remove();
             //lockID();
             window.setTimeout(function() {
                 isSafe = true;
             }, 1000);
         });
-    });
+    });*/
 
     function modeChange(n, o) {
         //lockID();
@@ -120,11 +154,11 @@ on  30/9/2017
         //console.log(JSON.stringify(n));
         var field = false;
         var value = true;
-        if(jsonData != null){
-            if(jsonData.locked == true)value = false;
+        if (jsonData != null) {
+            if (jsonData.locked == true) value = false;
         }
-        if(n.path.length == 1){
-            switch(n.path[0]){
+        if (n.path.length == 1) {
+            switch (n.path[0]) {
                 case "id":
                     value = false;
                     break;
@@ -133,7 +167,7 @@ on  30/9/2017
                     break;
             }
         } else {
-            switch(n.path[0]){
+            switch (n.path[0]) {
                 case "userData":
                 case "texturest":
                     field = true;
--- power.js
+++ power.js
@@ -19,17 +19,21 @@ on  30/9/2017
 
 */
 (function() {
+    var EDIT_SETTING = "io.highfidelity.isEditting"
+    var isEdit = Settings.getValue(EDIT_SETTING);
 
-
-    Messages.subscribe("selection-events");
-    Messages.subscribe("io.wlf.hifi.poweredit");
+    //Messages.subscribe("edit-events");
+    //Messages.subscribe("selection-events");
+    //Messages.subscribe("io.wlf.hifi.poweredit");
 
     Messages.messageReceived.connect(handleEntitySelectionToolUpdates);
 
-    function handleEntitySelectionToolUpdates(channel, message, sender) {
-        if (sender !== MyAvatar.sessionUUID){
-        	return;
+    function handleEntitySelectionToolUpdates(channel, message, sender, local) {
+        if (sender !== MyAvatar.sessionUUID) {
+            return;
         }
+
+        //console.log("Message",channel, message, sender, local);
 
         var json;
         try {
@@ -38,16 +42,21 @@ on  30/9/2017
             print("error -- gotWrongSelectMessage: " + message);
             return;
         }
-        if(channel === "selection-events"){
-        	if (json.action == "selected") {
-            	sendData(json.ids[0]);
-        	}
-    	} else if (channel === "io.wlf.hifi.poweredit"){
-    		if(json.action == "poweredit"){
-    			sendData(json.id);
-    			setWebState(true);
-    		}
-    	}
+        if (channel === "selection-events") {
+            if (json.action == "selected") {
+                //console.log("json send data");
+                sendData(json.ids[0]);
+            }
+        } else if (channel === "io.wlf.hifi.poweredit") {
+            if (json.action == "poweredit") {
+                sendData(json.id);
+                setWebState(true);
+            }
+        } else if (channel === "edit-events") {
+            if (json.hasOwnProperty("enabled")) {
+                isEdit = json.enabled;
+            }
+        }
     }
 
     var html = Script.resolvePath("html/editor.html?" + Date.now());
@@ -77,101 +86,104 @@ on  30/9/2017
     //print(JSON.stringify(Object.keys(editorUI)));
 
     function toggleWebState() {
-    	setWebState(!isActive);
+        setWebState(!isActive);
     }
 
-    function setWebState(on){
-    	if(on === isActive)return;
-    	isActive = on;
+    function setWebState(on) {
+        if (on === isActive) return;
+        isActive = on;
         editorUI.setVisible(isActive);
         button.editProperties({ isActive: isActive });
     }
 
     button.clicked.connect(toggleWebState);
 
-    editorUI.visibleChanged.connect(function(){
-    	setWebState(editorUI.isVisible());
+    editorUI.visibleChanged.connect(function() {
+        setWebState(editorUI.isVisible());
     });
 
     editorUI.webEventReceived.connect(function(msg) {
         msg = JSON.parse(msg);
-        if(msg.type === "picker"){
-        	isPicker = true;
-        } else if(msg.type === "jsonData") {
-        	//print(JSON.stringify(msg));
-        	if(msg.data.props.hasOwnProperty("id")){
-        		delete msg.data.props["id"];
-        	}
+        if (msg.type === "picker") {
+            isPicker = true;
+        } else if (msg.type === "jsonData") {
+            //print(JSON.stringify(msg));
+            if (msg.data.props.hasOwnProperty("id")) {
+                delete msg.data.props["id"];
+            }
 
-    		msg.data.props = stringify(msg.data.props,["userData","originalTextures","textures"]);
-        	Entities.editEntity(msg.data.id,msg.data.props);
-        } else if(msg.type === "jsonDataRequest"){
-        	sendData(msg.data);
+            msg.data.props = stringify(msg.data.props, ["userData", "originalTextures", "textures"]);
+            Entities.editEntity(msg.data.id, msg.data.props);
+        } else if (msg.type === "jsonDataRequest") {
+            sendData(msg.data);
         }
     });
 
 
     Script.scriptEnding.connect(function() {
-    	Controller.mouseReleaseEvent.disconnect(mouseReleaseEvent);
+        Controller.mouseReleaseEvent.disconnect(mouseReleaseEvent);
         button.clicked.disconnect(toggleWebState);
         tablet.removeButton(button);
+        editorUI.close();
     });
 
 
 
     function sendScriptEvent(type, data) {
+        //console.log("SEND SCRIPT EVENT",type,data);
         if (editorUI != null) {
+            console.log("SENDING", type, data);
             editorUI.emitScriptEvent(JSON.stringify({ type: type, data: data }));
         }
     }
 
     function mouseReleaseEvent(event) {
-        if (!isActive || !isPicker){return;}
+        if (!isEdit && !isPicker) { return; }
         isPicker = false;
         var clicked = findClickedEntity(event);
-        if(clicked === null || clicked === undefined){return;}
+        if (clicked === null || clicked === undefined) { return; }
         sendData(clicked.entityID);
     }
 
-    function objectify(obj,tags){
-    	if(!(tags instanceof Array))tags = [tags];
-    	for(var i = 0;i < tags.length;i++){
-    		var tag = tags[i];
-    		if(obj.hasOwnProperty(tag)){
-    			if(typeof obj[tag] === "string"){
-    				try {
-    	        		obj[tag] = JSON.parse(obj[tag]);
-	        		} catch (err) {
+    function objectify(obj, tags) {
+        if (!(tags instanceof Array)) tags = [tags];
+        for (var i = 0; i < tags.length; i++) {
+            var tag = tags[i];
+            if (obj.hasOwnProperty(tag)) {
+                if (typeof obj[tag] === "string") {
+                    try {
+                        obj[tag] = JSON.parse(obj[tag]);
+                    } catch (err) {
 
-        			}
-        		}
-    		}
-    	}
-    	return obj;
+                    }
+                }
+            }
+        }
+        return obj;
     }
 
-    function stringify(obj,tags){
-    	if(!(tags instanceof Array))tags = [tags];
-    	for(var i = 0;i < tags.length;i++){
-    		var tag = tags[i];
-    		if(obj.hasOwnProperty(tag)){
-    			if(typeof obj[tag] === "object"){
-    				obj[tag] = JSON.stringify(obj[tag]);
-    			}
-    		}
-    	}
-    	return obj;
+    function stringify(obj, tags) {
+        if (!(tags instanceof Array)) tags = [tags];
+        for (var i = 0; i < tags.length; i++) {
+            var tag = tags[i];
+            if (obj.hasOwnProperty(tag)) {
+                if (typeof obj[tag] === "object") {
+                    obj[tag] = JSON.stringify(obj[tag]);
+                }
+            }
+        }
+        return obj;
     }
 
-    function sendData(id){
-    	var data = Entities.getEntityProperties(id);
+    function sendData(id) {
+        var data = Entities.getEntityProperties(id);
 
-    	data = objectify(data,["userData","originalTextures","textures"]);
-    	//data.userData = JSON.parse(data.userData);
+        data = objectify(data, ["userData", "originalTextures", "textures"]);
+        //data.userData = JSON.parse(data.userData);
 
-    	data.locked = data.locked == 1;
-    	data.dynamic = data.dynamic == 1;
-    	data.visible = data.visible == 1;
+        data.locked = data.locked == 1;
+        data.dynamic = data.dynamic == 1;
+        data.visible = data.visible == 1;
 
         sendScriptEvent("jsonData", data);
     }
@@ -204,7 +216,8 @@ on  30/9/2017
 
         /*if (iconResult.intersects) {
             result = iconResult;
-        } else */if (entityResult.intersects) {
+        } else */
+        if (entityResult.intersects) {
             result = entityResult;
         } else {
             return null;
